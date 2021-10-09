134b6acae613b6c0b9501fceaae7ef60e90713c1
Boierul <49337867+Boierul@users.noreply.github.com>
2021-10-09 18:56:00 +0200

    Initial commit

---
+++ Assignment1/Data/IAdult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assignment1.Models;
+
+namespace Assignment1.Data
+{
+    public interface IAdult
+    {
+        public IList<Person> Persons { get; }
+        public void AddNewPerson(Person person);
+    }
+}
---
+++ Assignment1/Data/Impl/Adult.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using Assignment1.Models;
+
+namespace Assignment1.Data.Impl
+{
+    public class Adult :  IAdult
+    {
+        public IList<Person> Persons { get; private set; }
+
+        private string personsFile = "Adults.json";
+
+        public Adult()
+        {
+            if (!File.Exists(personsFile))
+            {
+                Seed();
+                string personsAsJson = JsonSerializer.Serialize(Persons);
+                File.WriteAllText(personsFile, personsAsJson);
+            }
+            else
+            {
+                string content = File.ReadAllText(personsFile);
+                Persons = JsonSerializer.Deserialize<List<Person>>(content);
+            }
+        }
+
+        public void AddNewPerson(Person person)
+        {
+            Persons.Add(person);
+            WriteTodosToFile();
+        }
+        
+        private void WriteTodosToFile()
+        {
+            string todosAsJson = JsonSerializer.Serialize(Persons);
+            File.WriteAllText(personsFile, todosAsJson);
+        }
+        
+        private void Seed()
+        {
+            Person[] ps =
+            {
+                new Person()
+                {
+                    Id = 1,
+                    FirstName = "James",
+                    LastName = "Charles",
+                    HairColor = "black",
+                    EyeColor = "blue",
+                    Age = 22,
+                    Weight = 72,
+                    Sex = "M"
+                },
+                new Person()
+                {
+                    Id = 2,
+                    FirstName = "Shrek",
+                    LastName = "Shrek",
+                    HairColor = "blonde",
+                    EyeColor = "green",
+                    Age = 52,
+                    Weight = 169,
+                    Sex = "M"
+                }
+            };
+        
+            Persons = ps.ToList();
+        }
+        
+    }
+}
+
